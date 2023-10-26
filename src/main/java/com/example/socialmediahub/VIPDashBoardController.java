67861f24054ee5e4f3189f22a8ee90bb8a6c9c66
BrodieLamont <lamontrb@gmail.com>
2023-10-27 03:21:11 +1100

    VIPDashboard fixes

--- src/main/java/com/example/socialmediahub/VIPDashBoardController.java
+++ src/main/java/com/example/socialmediahub/VIPDashBoardController.java
@@ -1,5 +1,39 @@
 package com.example.socialmediahub;
 
-public class VIPDashBoardController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VIPDashBoardController implements Initializable {
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Button buttonMenuLogout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        buttonMenuLogout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                UserDataBaseController.changeScene(actionEvent, "Log In", null, null, "login.fxml");
+            }
+        });
+    }
+
+    public void setWelcome(String firstname, String lastname){
+        firstNameLabel.setText(firstname);
+        lastNameLabel.setText(lastname);
+    }
 }
