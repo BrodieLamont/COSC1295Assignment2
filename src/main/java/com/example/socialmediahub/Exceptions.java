31419aaf9607e55220442e76111e9a6dcf913d88
BrodieLamont <lamontrb@gmail.com>
2023-10-27 00:36:18 +1100

    Added Exceptions class

---
+++ src/main/java/com/example/socialmediahub/Exceptions.java
@@ -0,0 +1,37 @@
+package com.example.socialmediahub;
+
+public class Exceptions {
+
+    static class IDException extends Exception{
+        /**
+         * Exception for non unique post ID.
+         *
+         * @param errorMessage Error message to display from Exception super class
+         */
+        public IDException(String errorMessage){
+            super(errorMessage);
+        }
+    }
+
+    static class IllegalNumber extends Exception{
+        /**
+         * Exception for illegal negative numbers.
+         *
+         * @param errorMessage Error message to display from Exception super class
+         */
+        public IllegalNumber(String errorMessage){
+            super(errorMessage);
+        }
+    }
+
+    static class DoesNotExist extends Exception{
+        /**
+         * Exception for when post object does not exist.
+         *
+         * @param errorMessage Error message to display from Exception super class
+         */
+        public DoesNotExist(String errorMessage){
+            super(errorMessage);
+        }
+    }
+}
