6e618ed81d5a3b7fcafbaa22bbc5defceed8532c
BrodieLamont <lamontrb@gmail.com>
2023-10-27 03:21:56 +1100

    Created log in function

--- src/main/java/com/example/socialmediahub/LoginController.java
+++ src/main/java/com/example/socialmediahub/LoginController.java
@@ -1,4 +1,45 @@
 package com.example.socialmediahub;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField enterLogInUsername;
+
+    @FXML
+    private TextField enterLogInPassword;
+
+    @FXML
+    private Button buttonLogIn;
+
+    @FXML
+    private Button buttonCreateAccount;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        buttonLogIn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+
+        buttonCreateAccount.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+
+    }
 }
