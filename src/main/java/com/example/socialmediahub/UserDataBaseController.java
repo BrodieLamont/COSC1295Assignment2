4e83023c46ed818e651e6cb096416fcc26a8510e
BrodieLamont <lamontrb@gmail.com>
2023-10-27 00:37:51 +1100

    Created UserDataBaseController to store users and processing logic

--- src/main/java/com/example/socialmediahub/UserDataBaseController.java
+++ src/main/java/com/example/socialmediahub/UserDataBaseController.java
@@ -1,4 +1,38 @@
 package com.example.socialmediahub;
 
+/*
+ * UserDatabase
+ *
+ * version 1.0
+ *
+ * 14/09/2023
+ *
+ * Copyright notice
+ */
+import java.util.*;
+/**
+ * Class for storing ArrayList of User objects.
+ * Contains method getUserDatabase() to return the database.
+ */
+
 public class UserDataBaseController {
+
+    private HashMap<String, RegularUser> regularDataBase = new HashMap<>();
+    private HashMap<String, VIPUser> vipDataBase = new HashMap<>();
+
+    public HashMap<String, RegularUser> getRegularDataBase(){
+        return regularDataBase;
+    }
+
+    public HashMap<String, VIPUser> getVipDataBase(){
+        return vipDataBase;
+    }
+
+    public void setRegularDataBase(HashMap<String, RegularUser> regularDataBase){
+        this.regularDataBase = regularDataBase;
+    }
+    public void setVipDataBase(HashMap<String, VIPUser> vipDataBase){
+        this.vipDataBase = vipDataBase;
+    }
+
 }
