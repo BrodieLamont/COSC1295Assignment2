6cd7ad7b534c7239a3d2a1c6da3a56449cb0a418
BrodieLamont <lamontrb@gmail.com>
2023-10-28 16:19:12 +1100

    removed unneeded files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,9 @@ module com.example.socialmediahub {
     opens com.example.socialmediahub to javafx.fxml;
     exports com.example.socialmediahub;
     exports com.example.socialmediahub.Controllers;
+    exports com.example.socialmediahub.Controllers.Users;
+    exports com.example.socialmediahub.Models;
+    exports com.example.socialmediahub.Views;
     opens com.example.socialmediahub.Controllers to javafx.fxml;
+    opens com.example.socialmediahub.Controllers.Users to javafx.fxml;
 }
