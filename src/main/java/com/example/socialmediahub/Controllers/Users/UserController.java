4e821fe6d023989381d57e17cd8a8fb9250acacc
BrodieLamont <lamontrb@gmail.com>
2023-10-29 11:03:52 +1100

    fixed bug in viewFactory not getting Anchors

--- src/main/java/com/example/socialmediahub/Controllers/Users/UserController.java
+++ src/main/java/com/example/socialmediahub/Controllers/Users/UserController.java
@@ -12,23 +12,16 @@ public class UserController implements Initializable {
     public BorderPane user_parent;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Model.getInstance().getViewFactory().getUserSelection().addListener((observable, oldValue, newValue) -> {
-            switch (newValue){
-                case "ViewPost":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getPostView());
-                case "AddPost":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getAddPost());
-                case "RemovePost":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getRemovePost());
-                case "Export":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getExportView());
-                case "Import":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getImportView());
-                case "Graph":
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getGraphView());
-                default:
-                    user_parent.setCenter(Model.getInstance().getViewFactory().getDashboardView());
-
+        Model.getInstance().getViewFactory().getUserSelection().addListener((observableValue, oldVal, newVal) -> {
+            switch (newVal){
+                case "ViewPost" -> user_parent.setCenter(Model.getInstance().getViewFactory().getPostView());
+                case "AddPost" -> user_parent.setCenter(Model.getInstance().getViewFactory().getAddPost());
+                case "RemovePost" -> user_parent.setCenter(Model.getInstance().getViewFactory().getRemovePost());
+                case "Export" -> user_parent.setCenter(Model.getInstance().getViewFactory().getExportView());
+                case "Import" -> user_parent.setCenter(Model.getInstance().getViewFactory().getImportView());
+                case "Graph" -> user_parent.setCenter(Model.getInstance().getViewFactory().getGraphView());
+                case "EditProfile" -> user_parent.setCenter(Model.getInstance().getViewFactory().getEditProfileView());
+                default -> user_parent.setCenter(Model.getInstance().getViewFactory().getDashboardView());
             }
         });
     }
